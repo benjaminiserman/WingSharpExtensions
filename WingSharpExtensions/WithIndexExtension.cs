e0cc64bf11420015dacbce0a60c211b4fd1a394d
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-27 15:09:44 -0600

    add WithIndex

---
+++ WingSharpExtensions/WithIndexExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace WingSharpExtensions;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class WithIndexExtension
+{
+	public static IEnumerable<(T Value, int Index)> WithIndex<T>(this IEnumerable<T> enumerable)
+	{
+		foreach (var t in enumerable.Select((x, i) => (x, i)))
+		{
+			yield return t;
+		}
+	}
+}
