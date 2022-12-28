91eaa7283a3fc355d630bbefd92eeb97fc95e57c
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-28 14:36:27 -0600

    add ToHashList extension method

---
+++ WingSharpExtensions/HashListExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace WingSharpExtensions;
+
+using System.Collections.Generic;
+
+public static class HashListExtension
+{
+	public static HashList<T> ToHashList<T>(this IEnumerable<T> enumerable) => new(enumerable);
+}
