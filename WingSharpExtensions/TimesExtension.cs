c8afe99aed9dddcc187e1948ab2fcabdbf8fd34f
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-28 15:33:06 -0600

    add TimesExtension

---
+++ WingSharpExtensions/TimesExtension.cs
@@ -0,0 +1,41 @@
+﻿namespace WingSharpExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TimesExtension
+{
+	public static IEnumerable<T> Times<T>(this Func<T> function, int times)
+	{
+		for (var i = 0; i < times; i++)
+		{
+			yield return function();
+		}
+	}
+
+	public static IEnumerable<T> Times<T>(this Func<int, T> function, int times)
+	{
+		for (var i = 0; i < times; i++)
+		{
+			yield return function(i);
+		}
+	}
+
+	public static void Times(this Action function, int times)
+	{
+		for (var i = 0; i < times; i++)
+		{
+			function();
+		}
+	}
+
+	public static void Times(this Action<int> function, int times)
+	{
+		for (var i = 0; i < times; i++)
+		{
+			function(i);
+		}
+	}
+}
