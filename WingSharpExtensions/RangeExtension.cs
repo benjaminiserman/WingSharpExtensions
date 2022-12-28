996f3fee7b4011b22b0194c02a5e12edf65040e4
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-27 18:19:16 -0600

    fix downwards ranges, add Range method

--- WingSharpExtensions/RangeExtension.cs
+++ WingSharpExtensions/RangeExtension.cs
@@ -2,25 +2,44 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 public static class RangeExtension
 {
-    public static IEnumerator<int> GetEnumerator(this Range range)
+    private static IEnumerator<int> RangeEnumerator(int start, int end)
     {
-        var (start, end) = (range.Start.Value, range.End.Value);
-        if (start < end)
+		foreach (var x in Range(start, end))
+		{
+			yield return x;
+		}
+	}
+
+	public static IEnumerable<int> Range(int start, int end)
+	{
+		if (start < end)
+		{
+			for (var i = start; i < end; i++)
+			{
+				yield return i;
+			}
+		}
+		else if (start > end)
+		{
+			for (var i = start; i > end; i--)
+			{
+				yield return i;
+			}
+		}
+	}
+
+	public static IEnumerator<int> GetEnumerator(this Range range) => RangeEnumerator(range.Start.Value, range.End.Value);
+
+
+	public static IEnumerable<int> Enumerate(this Range range)
+    {
+        foreach (var x in range)
         {
-            for (var i = start; i < end; i++)
-            {
-                yield return i;
-            }
-        }
-        else if (start > end)
-        {
-            for (var i = start; i > end; i++)
-            {
-                yield return i;
-            }
+            yield return x;
         }
     }
 }
