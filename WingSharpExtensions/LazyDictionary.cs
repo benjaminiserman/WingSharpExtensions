c139e228c70a13a573ce6870b62f180ab0f870c3
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-27 21:41:26 -0600

    copy dictionary in LazyDictionary ctor

--- WingSharpExtensions/LazyDictionary.cs
+++ WingSharpExtensions/LazyDictionary.cs
@@ -7,15 +7,19 @@ using System.Collections.Generic;
 public class LazyDictionary<T1, T2> : IDictionary<T1, T2>
 	where T1 : notnull
 {
-	private readonly Dictionary<T1, T2> _internalDictionary = new();
+	private readonly Dictionary<T1, T2> _internalDictionary;
 
 	public Func<T2>? GetDefault { get; init; } = null;
 	public bool AddMissingKeys { get; init; } = false;
 
-	public LazyDictionary() { }
+	public LazyDictionary() 
+	{
+		_internalDictionary = new();
+	}
+
 	public LazyDictionary(Dictionary<T1, T2> dictionary)
 	{
-		_internalDictionary = dictionary;
+		_internalDictionary = new(dictionary);
 	}
 
 	public T2 this[T1 key]
@@ -58,18 +62,18 @@ public class LazyDictionary<T1, T2> : IDictionary<T1, T2>
 	public ICollection<T2> Values => _internalDictionary.Values;
 	public int Count => _internalDictionary.Count;
 	public bool IsReadOnly => false;
-
+	
 	public void Add(T1 key, T2 value) => this[key] = value;
-	public void Add(KeyValuePair<T1, T2> item) => throw new NotImplementedException();
+	void ICollection<KeyValuePair<T1, T2>>.Add(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Add(item);
 	public void Clear() => _internalDictionary.Clear();
-	public bool Contains(KeyValuePair<T1, T2> item) => throw new NotImplementedException();
 	public bool ContainsKey(T1 key) => _internalDictionary.ContainsKey(key);
-	public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex) => throw new NotImplementedException();
 	IEnumerator<KeyValuePair<T1, T2>> IEnumerable<KeyValuePair<T1, T2>>.GetEnumerator() => _internalDictionary.GetEnumerator();
 	IEnumerator IEnumerable.GetEnumerator() => _internalDictionary.GetEnumerator();
 	public bool Remove(T1 key) => _internalDictionary.Remove(key);
 	public bool Remove(T1 key, out T2 value) => _internalDictionary.Remove(key, out value!);
-	public bool Remove(KeyValuePair<T1, T2> item) => throw new NotImplementedException();
 
 	public bool TryGetValue(T1 key, out T2 value) => _internalDictionary.TryGetValue(key, out value!);
+	public bool Contains(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Contains(item);
+	public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).CopyTo(array, arrayIndex);
+	public bool Remove(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Remove(item);
 }
