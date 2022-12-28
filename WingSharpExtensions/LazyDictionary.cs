74a0bdfe919f12aebdb8fbff400e074cb5e0ff85
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-28 14:44:32 -0600

    bring LazyDictionary constructors and extension methods to parity with Dictionary

--- WingSharpExtensions/LazyDictionary.cs
+++ WingSharpExtensions/LazyDictionary.cs
@@ -3,26 +3,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-public class LazyDictionary<T1, T2> : IDictionary<T1, T2>
-	where T1 : notnull
+public class LazyDictionary<TKey, TElement> : IDictionary<TKey, TElement>, IEnumerable<KeyValuePair<TKey, TElement>>
+	where TKey : notnull
 {
-	private readonly Dictionary<T1, T2> _internalDictionary;
+	private readonly Dictionary<TKey, TElement> _internalDictionary;
 
-	public Func<T2>? GetDefault { get; init; } = null;
+	public Func<TElement>? GetDefault { get; init; } = null;
 	public bool AddMissingKeys { get; init; } = false;
+	public IEqualityComparer<TKey> Comparer => _internalDictionary.Comparer;
 
-	public LazyDictionary() 
-	{
-		_internalDictionary = new();
-	}
+	public LazyDictionary() => _internalDictionary = new();
+	public LazyDictionary(int capacity) => _internalDictionary = new(capacity);
+	public LazyDictionary(IEqualityComparer<TKey> comparer) => _internalDictionary = new(comparer);
+	public LazyDictionary(int capacity, IEqualityComparer<TKey> comparer) => _internalDictionary = new(capacity, comparer);
+	public LazyDictionary(IEnumerable<KeyValuePair<TKey, TElement>> dictionary) => _internalDictionary = new(dictionary);
+	public LazyDictionary(IDictionary<TKey, TElement> dictionary) => _internalDictionary = new(dictionary);
+	public LazyDictionary(IEnumerable<KeyValuePair<TKey, TElement>> dictionary, IEqualityComparer<TKey> comparer) => _internalDictionary = new(dictionary, comparer);
+	public LazyDictionary(IDictionary<TKey, TElement> dictionary, IEqualityComparer<TKey> comparer) => _internalDictionary = new(dictionary, comparer);
+	internal LazyDictionary(Dictionary<TKey, TElement> dictionary) => _internalDictionary = dictionary;
 
-	public LazyDictionary(Dictionary<T1, T2> dictionary)
-	{
-		_internalDictionary = new(dictionary);
-	}
-
-	public T2 this[T1 key]
+	public TElement this[TKey key]
 	{
 		get
 		{
@@ -58,22 +60,22 @@ public class LazyDictionary<T1, T2> : IDictionary<T1, T2>
 		}
 	}
 
-	public ICollection<T1> Keys => _internalDictionary.Keys;
-	public ICollection<T2> Values => _internalDictionary.Values;
+	public ICollection<TKey> Keys => _internalDictionary.Keys;
+	public ICollection<TElement> Values => _internalDictionary.Values;
 	public int Count => _internalDictionary.Count;
 	public bool IsReadOnly => false;
 	
-	public void Add(T1 key, T2 value) => this[key] = value;
-	void ICollection<KeyValuePair<T1, T2>>.Add(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Add(item);
+	public void Add(TKey key, TElement value) => this[key] = value;
+	void ICollection<KeyValuePair<TKey, TElement>>.Add(KeyValuePair<TKey, TElement> item) => ((ICollection<KeyValuePair<TKey, TElement>>)_internalDictionary).Add(item);
 	public void Clear() => _internalDictionary.Clear();
-	public bool ContainsKey(T1 key) => _internalDictionary.ContainsKey(key);
-	IEnumerator<KeyValuePair<T1, T2>> IEnumerable<KeyValuePair<T1, T2>>.GetEnumerator() => _internalDictionary.GetEnumerator();
+	public bool ContainsKey(TKey key) => _internalDictionary.ContainsKey(key);
+	IEnumerator<KeyValuePair<TKey, TElement>> IEnumerable<KeyValuePair<TKey, TElement>>.GetEnumerator() => _internalDictionary.GetEnumerator();
 	IEnumerator IEnumerable.GetEnumerator() => _internalDictionary.GetEnumerator();
-	public bool Remove(T1 key) => _internalDictionary.Remove(key);
-	public bool Remove(T1 key, out T2 value) => _internalDictionary.Remove(key, out value!);
+	public bool Remove(TKey key) => _internalDictionary.Remove(key);
+	public bool Remove(TKey key, out TElement value) => _internalDictionary.Remove(key, out value!);
 
-	public bool TryGetValue(T1 key, out T2 value) => _internalDictionary.TryGetValue(key, out value!);
-	public bool Contains(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Contains(item);
-	public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).CopyTo(array, arrayIndex);
-	public bool Remove(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)_internalDictionary).Remove(item);
+	public bool TryGetValue(TKey key, out TElement value) => _internalDictionary.TryGetValue(key, out value!);
+	public bool Contains(KeyValuePair<TKey, TElement> item) => ((ICollection<KeyValuePair<TKey, TElement>>)_internalDictionary).Contains(item);
+	public void CopyTo(KeyValuePair<TKey, TElement>[] array, int arrayIndex) => ((ICollection<KeyValuePair<TKey, TElement>>)_internalDictionary).CopyTo(array, arrayIndex);
+	public bool Remove(KeyValuePair<TKey, TElement> item) => ((ICollection<KeyValuePair<TKey, TElement>>)_internalDictionary).Remove(item);
 }
---
+++ WingSharpExtensions/LazyDictionaryExtension.cs
@@ -0,0 +1,41 @@
+﻿namespace WingSharpExtensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class LazyDictionaryExtension
+{
+	public static LazyDictionary<TKey, TElement> ToLazyDictionary<TKey, TElement>(
+		this IEnumerable<KeyValuePair<TKey, TElement>> enumerable) 
+		where TKey : notnull 
+		=> new(enumerable);
+
+	public static LazyDictionary<TKey, TElement> ToLazyDictionary<TSource, TKey, TElement>(
+		this IEnumerable<TSource> enumerable,
+		Func<TSource, TKey> keySelector,
+		Func<TSource, TElement> elementSelector,
+		IEqualityComparer<TKey> comparer)
+		where TKey : notnull 
+		=> new(enumerable.ToDictionary(keySelector, elementSelector, comparer));
+
+	public static LazyDictionary<TKey, TElement> ToLazyDictionary<TSource, TKey, TElement>(
+		this IEnumerable<TSource> enumerable,
+		Func<TSource, TKey> keySelector,
+		Func<TSource, TElement> elementSelector)
+		where TKey : notnull
+		=> new(enumerable.ToDictionary(keySelector, elementSelector));
+
+	public static LazyDictionary<TKey, TSource> ToLazyDictionary<TSource, TKey>(
+		this IEnumerable<TSource> enumerable,
+		Func<TSource, TKey> keySelector,
+		IEqualityComparer<TKey> comparer)
+		where TKey : notnull
+		=> new(enumerable.ToDictionary(keySelector, comparer));
+
+	public static LazyDictionary<TKey, TSource> ToLazyDictionary<TSource, TKey>(
+		this IEnumerable<TSource> enumerable,
+		Func<TSource, TKey> keySelector)
+		where TKey : notnull
+		=> new(enumerable.ToDictionary(keySelector));
+}
