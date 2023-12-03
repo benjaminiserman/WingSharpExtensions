d9244902e99e29ba3d287dffd643846a64fd3e31
Benjamin E. Iserman <winggar1228@gmail.com>
2023-12-02 16:47:30 -0800

    added IList destructuring

---
+++ WingSharpExtensions/ArrayDestructuringExtension.cs
@@ -0,0 +1,82 @@
+﻿namespace WingSharpExtensions;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArrayDestructuringExtension
+{
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T[] rest)
+	{
+		item1 = list[0];
+		rest = list.Skip(1).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		rest = list.Skip(2).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		rest = list.Skip(3).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T item4, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		item4 = list[3];
+		rest = list.Skip(4).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T item4, out T item5, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		item4 = list[3];
+		item5 = list[4];
+		rest = list.Skip(5).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T item4, out T item5, out T item6, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		item4 = list[3];
+		item5 = list[4];
+		item6 = list[5];
+		rest = list.Skip(6).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T item4, out T item5, out T item6, out T item7, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		item4 = list[3];
+		item5 = list[4];
+		item6 = list[5];
+		item7 = list[6];
+		rest = list.Skip(7).ToArray();
+	}
+
+	public static void Deconstruct<T>(this IList<T> list, out T item1, out T item2, out T item3, out T item4, out T item5, out T item6, out T item7, out T item8, out T[] rest)
+	{
+		item1 = list[0];
+		item2 = list[1];
+		item3 = list[2];
+		item4 = list[3];
+		item5 = list[4];
+		item6 = list[5];
+		item7 = list[6];
+		item8 = list[7];
+		rest = list.Skip(8).ToArray();
+	}
+}
---
+++ WingSharpExtensionsTest/ArrayDestructuringTests.cs
@@ -0,0 +1,140 @@
+﻿namespace WingSharpExtensionsTest;
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingSharpExtensions;
+
+[TestClass]
+public class ArrayDestructuringTests
+{
+	private List<int> GetTestList() => new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	[TestMethod]
+	public void Destructure_One_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.IsTrue(rest is [2, 3, 4, 5, 6, 7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Two_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.IsTrue(rest is [3, 4, 5, 6, 7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Three_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.IsTrue(rest is [4, 5, 6, 7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Four_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, d, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.AreEqual(d, list[3]);
+		Assert.IsTrue(rest is [5, 6, 7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Five_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, d, e, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.AreEqual(d, list[3]);
+		Assert.AreEqual(e, list[4]);
+		Assert.IsTrue(rest is [6, 7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Six_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, d, e, f, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.AreEqual(d, list[3]);
+		Assert.AreEqual(e, list[4]);
+		Assert.AreEqual(f, list[5]);
+		Assert.IsTrue(rest is [7, 8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Seven_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, d, e, f, g, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.AreEqual(d, list[3]);
+		Assert.AreEqual(e, list[4]);
+		Assert.AreEqual(f, list[5]);
+		Assert.AreEqual(g, list[6]);
+		Assert.IsTrue(rest is [8, 9]);
+	}
+
+	[TestMethod]
+	public void Destructure_Eight_CorrectResult()
+	{
+		var list = GetTestList();
+
+		var (a, b, c, d, e, f, g, h, rest) = list;
+
+		Assert.AreEqual(a, list[0]);
+		Assert.AreEqual(b, list[1]);
+		Assert.AreEqual(c, list[2]);
+		Assert.AreEqual(d, list[3]);
+		Assert.AreEqual(e, list[4]);
+		Assert.AreEqual(f, list[5]);
+		Assert.AreEqual(g, list[6]);
+		Assert.AreEqual(h, list[7]);
+		Assert.IsTrue(rest is [9]);
+	}
+
+	[TestMethod]
+	public void Destructure_EmptySequence_Throws()
+	{
+		var list = new List<int>();
+
+		Assert.ThrowsException<ArgumentOutOfRangeException>(() => 
+		{
+			var (a, b, c, _) = list;
+			Console.WriteLine(a + b + c);
+		});
+	}
+}
