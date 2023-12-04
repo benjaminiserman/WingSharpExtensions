43a117570f1e5fd06955d6d8a714fb03d0e456ff
Benjamin E. Iserman <winggar1228@gmail.com>
2023-12-03 23:40:16 -0800

    add Memoize function

---
+++ WingSharpExtensions/MemoizeFactory.cs
@@ -0,0 +1,111 @@
+﻿namespace WingSharpExtensions;
+
+using System;
+using System.Collections.Generic;
+
+public static class MemoizeFactory
+{
+	public static Func<TResult> Memoize<TResult>(this Func<TResult> function)
+	{
+		TResult? result = default;
+		var calculated = false;
+
+		TResult WrappedFunction()
+		{
+			if (!calculated)
+			{
+				result = function();
+				calculated = true;
+			}
+
+			return result!;
+		}
+
+		return WrappedFunction;
+	}
+
+	public static Func<T1, TResult> Memoize<T1, TResult>(this Func<T1, TResult> function)
+		where T1 : notnull
+	{
+		var memoizedResults = new Dictionary<T1, TResult>();
+		
+		TResult WrappedFunction(T1 arg1)
+		{
+			if (memoizedResults.TryGetValue(arg1, out var value))
+			{
+				return value;
+			}
+			else
+			{
+				var result = function(arg1);
+				memoizedResults.Add(arg1, result);
+				return result;
+			}
+		}
+
+		return WrappedFunction;
+	}
+
+	public static Func<T1, T2, TResult> Memoize<T1, T2, TResult>(this Func<T1, T2, TResult> function)
+	{
+		var memoizedResults = new Dictionary<(T1, T2), TResult>();
+
+		TResult WrappedFunction(T1 arg1, T2 arg2)
+		{
+			if (memoizedResults.TryGetValue((arg1, arg2), out var value))
+			{
+				return value;
+			}
+			else
+			{
+				var result = function(arg1, arg2);
+				memoizedResults.Add((arg1, arg2), result);
+				return result;
+			}
+		}
+
+		return WrappedFunction;
+	}
+
+	public static Func<T1, T2, T3, TResult> Memoize<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> function)
+	{
+		var memoizedResults = new Dictionary<(T1, T2, T3), TResult>();
+
+		TResult WrappedFunction(T1 arg1, T2 arg2, T3 arg3)
+		{
+			if (memoizedResults.TryGetValue((arg1, arg2, arg3), out var value))
+			{
+				return value;
+			}
+			else
+			{
+				var result = function(arg1, arg2, arg3);
+				memoizedResults.Add((arg1, arg2, arg3), result);
+				return result;
+			}
+		}
+
+		return WrappedFunction;
+	}
+
+	public static Func<T1, T2, T3, T4, TResult> Memoize<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> function)
+	{
+		var memoizedResults = new Dictionary<(T1, T2, T3, T4), TResult>();
+
+		TResult WrappedFunction(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+		{
+			if (memoizedResults.TryGetValue((arg1, arg2, arg3, arg4), out var value))
+			{
+				return value;
+			}
+			else
+			{
+				var result = function(arg1, arg2, arg3, arg4);
+				memoizedResults.Add((arg1, arg2, arg3, arg4), result);
+				return result;
+			}
+		}
+
+		return WrappedFunction;
+	}
+}
---
+++ WingSharpExtensionsTest/MemoizeTests.cs
@@ -0,0 +1,130 @@
+﻿namespace WingSharpExtensionsTest;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingSharpExtensions;
+
+[TestClass]
+public class MemoizeTests
+{
+	[TestMethod]
+	public void Memoize_NoArgs_CorrectResult()
+	{
+		var memoizedFunction = MemoizeFactory.Memoize(() => new Random().NextDouble());
+
+		var firstResult = memoizedFunction();
+		var secondResult = memoizedFunction();
+
+		Assert.AreEqual(firstResult, secondResult);
+	}
+
+	[TestMethod]
+	public void Memoize_Unary_CorrectResult()
+	{
+		var i = 1;
+		int BadFunction(int x)
+		{
+			i++;
+			return i * x;
+		}
+
+		var memoizedBadFunction = MemoizeFactory.Memoize<int, int>(BadFunction);
+		var firstResult = memoizedBadFunction(5);
+		var secondResult = memoizedBadFunction(5);
+
+		Assert.AreEqual(firstResult, secondResult);
+	}
+
+	[TestMethod]
+	public void Memoize_Binary_CorrectResult()
+	{
+		Random random = new();
+		int BadFunction(int a, int b)
+		{
+			return new List<int>()
+			{
+				a * random.Next(),
+				b * random.Next()
+			}.Aggregate((a, b) => a * b);
+		}
+
+		var memoizedBadFunction = MemoizeFactory.Memoize<int, int, int>(BadFunction);
+		var firstResult = memoizedBadFunction(1, 2);
+		var secondResult = memoizedBadFunction(1, 2);
+
+		Assert.AreEqual(firstResult, secondResult);
+	}
+
+	[TestMethod]
+	public void Memoize_Ternary_CorrectResult()
+	{
+		Random random = new();
+		int BadFunction(int a, int b, int c)
+		{
+			return new List<int>()
+			{
+				a * random.Next(),
+				b * random.Next(),
+				c * random.Next()
+			}.Aggregate((a, b) => a * b);
+		}
+
+		var memoizedBadFunction = MemoizeFactory.Memoize<int, int, int, int>(BadFunction);
+		var firstResult = memoizedBadFunction(1, 2, 3);
+		var secondResult = memoizedBadFunction(1, 2, 3);
+
+		Assert.AreEqual(firstResult, secondResult);
+	}
+
+	[TestMethod]
+	public void Memoize_Quaternary_CorrectResult()
+	{
+		Random random = new();
+		int BadFunction(int a, int b, int c, int d)
+		{
+			return new List<int>()
+			{
+				a * random.Next(),
+				b * random.Next(),
+				c * random.Next(),
+				d * random.Next()
+			}.Aggregate((a, b) => a * b);
+		}
+
+		var myDictionary = new Dictionary<List<int>, int>();
+		var test = MemoizeFactory.Memoize<List<int>, int>(a => 1);
+
+		var memoizedBadFunction = MemoizeFactory.Memoize<int, int, int, int, int>(BadFunction);
+		var firstResult = memoizedBadFunction(1, 2, 3, 4);
+		var secondResult = memoizedBadFunction(1, 2, 3, 4);
+
+		Assert.AreEqual(firstResult, secondResult);
+	}
+
+	[TestMethod, Timeout(1000)]
+	public void Memoize_Fibonacci()
+	{
+		Func<BigInteger, BigInteger> fibonacci = x => x;
+		fibonacci = (BigInteger x) =>
+		{
+			if (x == 0)
+			{
+				return 0;
+			} 
+			else if (x == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return fibonacci(x - 2) + fibonacci(x - 1);
+			}
+		};
+		fibonacci = fibonacci.Memoize();
+
+		Assert.AreEqual(fibonacci(50), new BigInteger(12586269025));
+	}
+}
